25d7ea0fa70218df803a25194e325fd3d0dbd5c7
pathgit31 <tanaypathakamuri@gmail.com>
2025-06-24 23:50:41 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
   res.setHeader('Content-Type', 'application/xml');
-  res.status(200).send(`<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-  <Say>Hello! Your call has been received.</Say>
-</Response>`);
+  res.status(200).send(`
+    <Response>
+      <Redirect method="POST">https://pathn8n.app.n8n.cloud/webhook/start-call</Redirect>
+    </Response>
+  `);
 }
