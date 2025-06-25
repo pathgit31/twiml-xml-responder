dcdd873c7fac0d3cf37635b3c7668eeb57e92f54
pathgit31 <tanaypathakamuri@gmail.com>
2025-06-24 23:57:31 -0400

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,9 @@
 export default function handler(req, res) {
-  res.setHeader('Content-Type', 'application/xml');
+  res.setHeader('Content-Type', 'text/xml'); // ✅ REQUIRED by Twilio
   res.status(200).send(`
     <Response>
       <Redirect method="POST">https://pathn8n.app.n8n.cloud/webhook/start-call</Redirect>
     </Response>
   `);
 }
+
